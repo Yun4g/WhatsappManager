34ff4a8e8af8fbdf97bbda3dbf0e0b30f88c93b4
Delight Aberage <delightvincent487@gmail.com>
2026-03-21 12:04:20 +0100

    push update

--- src/store/dashboardStore.ts
+++ src/store/dashboardStore.ts
@@ -1,12 +1,21 @@
 import { create } from "zustand";
 
+
+interface UserDataType {
+  userId: string
+}
 interface DashboardStore {
   isConnected: boolean;
+  userData: UserDataType[];
   setIsConnected: (isConnected: boolean) => void;
+  setUserData: (userData: UserDataType[]) => void; 
 }
 
 export const useDashboardStore = create<DashboardStore>((set) => ({
   isConnected: false,
+  userData: [],
+  setUserData: (userData: UserDataType[]) =>
+    set({ userData }),
 
   setIsConnected: (isConnected: boolean) =>
     set({ isConnected }),
