d9bb342119778145242486ca92601c7fd2acf999
Delight Aberage <delightvincent487@gmail.com>
2026-03-26 10:52:24 +0100

    push update

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,19 +1,29 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export const getUser = async () => {
   try {
-    const res = await axios.get('https://manajer-22u7.onrender.com/auth/get-user', {
-      withCredentials: true,
-    });
+    const res = await axios.get(
+      "https://manajer-22u7.onrender.com/auth/get-user",
+      { withCredentials: true }
+    );
 
-
-    return res.data; 
+    return res.data;
   } catch (error) {
-     if (axios.isAxiosError(error) && error.response?.status === 401) {
-      // window.location.href = '/';
-      return null;
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        console.log("Session expired");
+        toast.error("Session expired. Please login again.");
+
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 1000);
+
+        return null;
+      }
     }
+
     console.log(error);
-    return null; 
+    return null;
   }
 };
